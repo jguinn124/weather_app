4b0b63d990c181857292eec7d9404db34208a23c
John Guinn <jguinn124@gmail.com>
2020-03-29 17:13:22 -0500

    Styling

--- src/App.js
+++ src/App.js
@@ -1,8 +1,22 @@
 import React from 'react';
 
+const api = {
+	key: "b3beb547b4350f89d8920d06161e4080",
+	base: "https://api.openweathermap.org/data/2.5/"
+}
+
 function App() {
   return (
-    <div className="App">
+    <div className="app">
+		<main>
+			<div className="search-box">
+				<input 
+				type="text"
+				className="search-bar"
+				placeholder="Search"
+				/>
+			</div>
+		</main>
       
     </div>
   );
