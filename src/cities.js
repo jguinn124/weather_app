9cb4d90630aea8a626ac6ee25406f55ef58ce167
John Guinn <jguinn124@gmail.com>
2020-04-15 11:08:24 -0500

    Added files for auto complete search

---
+++ src/cities.js
@@ -0,0 +1,358 @@
+export default`Aberdeen
+Abilene, TX, US
+Akron, OH, US
+Albany, NY, US
+Albuquerque, NM, US
+Alexandria, LA, US
+Allentown, PA, US
+Amarillo, TX, US
+Anaheim, CA, US
+Anchorage, AK, US
+Ann Arbor, MI, US
+Antioch, CA, US
+Apple Valley, CA, US
+Appleton, WI, US
+Arlington, TX, US
+Arvada, CO, US
+Asheville, NC, US
+Athens, GA, US
+Atlanta, GA, US
+Atlantic City, NJ, US
+Augusta, GA, US
+Aurora, CO, US
+Austin, TX, US
+Bakersfield, CA, US
+Baltimore, MD, US
+Barnstable, MA, US
+Baton Rouge, LA, US
+Beaumont, TX, US
+Bel Air, MD, US
+Bellevue, WA, US
+Berkeley, CA, US
+Bethlehem, PA, US
+Billings, MT, US
+Birmingham, AL, US
+Bloomington, IL, US
+Boise, ID, US
+Boise City, ID, US
+Bonita Springs, FL, US
+Boston, MA, US
+Boulder, CO, US
+Bradenton, FL, US
+Bremerton, WA, US
+Bridgeport, CT, US
+Brighton, IL, US
+Brownsville, TX, US
+Bryan, TX, US
+Buffalo, NY, US
+Burbank, CA, US
+Burlington, VT, US
+Cambridge, MA, US
+Canton, OH, US
+Cape Coral, FL, US
+Carrollton, TX, US
+Cary, NC, US
+Cathedral City, CA, US
+Cedar Rapids, IA, US
+Champaign, IL, US
+Chandler, AZ, US
+Charleston, SC, US
+Charlotte, NC, US
+Chattanooga, TN, US
+Chesapeake, VA, US
+Chicago, IL, US
+Chula Vista, CA, US
+Cincinnati, OH, US
+Clarke County, AL, US
+Clarksville, TN, US
+Clearwater, OK, US
+Cleveland, OH, US
+College Station, TX, US
+Colorado Springs, CO, US
+Columbia, MO, US
+Columbia, SC, US
+Columbus, OH, US
+Concord, CA, US
+Coral Springs, FL, US
+Corona, CA, US
+Corpus Christi, TX, US
+Costa Mesa, CA, US
+Dallas, TX, US
+Daly City, CA, US
+Danbury, CT, US
+Davenport, IA, US
+Davidson County, TN, US
+Dayton, OH, US
+Daytona Beach, FL, US
+Deltona, FL, US
+Denton, TX, US
+Denver, CO, US
+Des Moines, IA, US
+Detroit, MI, US
+Downey, CA, US
+Duluth, MN, US
+Durham, NC, US
+El Monte, CA, US
+El Paso, TX, US
+Elizabeth, NJ, US
+Elk Grove, CA, US
+Elkhart, IN, US
+Erie, PA, US
+Escondido, CA, US
+Eugene, OR, US
+Evansville, IN, US
+Fairfield, OH, US
+Fargo, ND, US
+Fayetteville, NC, US
+Fitchburg, MA, US
+Flint, MI, US
+Fontana, CA, US
+Fort Collins, CO, US
+Fort Lauderdale, FL, US
+Fort Smith, AR, US
+Fort Walton Beach, FL, US
+Fort Wayne, IN, US
+Fort Worth, TX, US
+Frederick, MD, US
+Fremont, CA, US
+Fresno, CA, US
+Fullerton, CA, US
+Gainesville, FL, US
+Garden Grove, CA, US
+Garland, TX, US
+Gastonia, NC, US
+Gilbert, AZ, US
+Glendale, AZ, US
+Grand Prairie, TX, US
+Grand Rapids, MI, US
+Grayslake, IL, US
+Green Bay, WI, US
+Greensboro, NC, US
+Greenville, SC, US
+Gulfport-Biloxi, MS, US
+Hagerstown, MD, US
+Hampton, VA, US
+Harlingen, TX, US
+Harrisburg, PA, US
+Hartford, CT, US
+Havre de Grace, MD, US
+Hayward, CA, US
+Hemet, CA, US
+Henderson, NV, US
+Hesperia, CA, US
+Hialeah, FL, US
+Hickory, NC, US
+High Point, NC, US
+Hollywood, CA, US
+Honolulu, HI, US
+Houma, LA, US
+Houston, TX, US
+Howell, MI, US
+Huntington, WV, US
+Huntington Beach, CA, US
+Huntsville, AL, US
+Independence, MO, US
+Indianapolis, IN, US
+Inglewood, CA, US
+Irvine, CA, US
+Irving, TX, US
+Jackson, MS, US
+Jacksonville, FL, US
+Jefferson, WI, US
+Jersey City, NJ, US
+Johnson City, TN, US
+Joliet, IL, US
+Kailua, HI, US
+Kalamazoo, MI, US
+Kaneohe, HI, US
+Kansas City, MO, US
+Kennewick, WA, US
+Kenosha, WI, US
+Killeen, TX, US
+Kissimmee, FL, US
+Knoxville, TN, US
+Lacey, WA, US
+Lafayette, LA, US
+Lake Charles, LA, US
+Lakeland, Fl, US
+Lakewood, CO, US
+Lancaster, PA, US
+Lansing, MI, US
+Laredo, TX, US
+Las Cruces, NM, US
+Las Vegas, NV, US
+Layton, UT, US
+Leominster, MA, US
+Lewisville, TX, US
+Lexington, KY, US
+Lincoln, NE, US
+Little Rock, AR, US
+Long Beach, CA, US
+Lorain, OH, US
+Los Angeles, CA, US
+Louisville, KY, US
+Lowell, MA, US
+Lubbock, TX, US
+Macon, GA, US
+Madison, WI, US
+Manchester, MO, US
+Marina, CA, US
+Marysville, WA, US
+McAllen, TX, US
+McHenry, IL, US
+Medford, OR, US
+Melbourne, FL, US
+Memphis, TN, US
+Merced, CA, US
+Mesa, AZ, US
+Mesquite, NV, US
+Miami, FL, US
+Milwaukee, WI, US
+Minneapolis, MN, US
+Miramar, FL, US
+Mission Viejo, CA, US
+Mobile, AL, US
+Modesto, CA, US
+Monroe, LA, US
+Monterey, CA, US
+Montgomery, AL, US
+Moreno Valley, CA, US
+Myrtle Beach, SC, US
+Naperville, IL, US
+Naples, FL, US
+Nashua, NH, US
+Nashville, TN, US
+New Bedford, MA, US
+New Haven, CT, US
+New London, CT, US
+New Orleans, LA, US
+New York City, NY, US
+Newark, NJ, US
+Newburgh, NY, US
+Newport News, VA, US
+Norfolk, VA, US
+Normal, IL, US
+Norman, OK, US
+North Charleston, SC, US
+Norwalk, CT, US
+Norwich, CT, US
+Oakland, CA, US
+Ocala, FL, US
+Oceanside, CA, US
+Odessa, MO, US
+Ogden, UT, US
+Oklahoma City, OK, US
+Olathe, KS, US
+Olympia, WA, US
+Omaha, NE, US
+Orem, UT, US
+Orlando, FL, US
+Overland Park, KS, US
+Oxnard, CA, US
+Palm Springs, CA, US
+Palmdale, CA, US
+Panama City, FL, US
+Pasadena, CA, US
+Paterson, NJ, US
+Pensacola, FL, US
+Peoria, IL, US
+Philadelphia, PA, US
+Phoenix, AZ, US
+Pittsburgh, PA, US
+Plano, TX, US
+Pomona, CA, US
+Pompano Beach, FL, US
+Port Arthur, TX, US
+Port Saint Lucie, FL, US
+Portland, OR, US
+Portsmouth, NH, US
+Poughkeepsie, NY, US
+Providence, RI, US
+Provo, UT, US
+Pueblo, CO, US
+Punta Gorda, FL, US
+Racine, WI, US
+Raleigh, NC, US
+Redding, CA, US
+Reno, NV, US
+Richland, WA, US
+Richmond, VA, US
+Riverside, CA, US
+Roanoke, VA, US
+Rochester, NY, US
+Rockford, IL, US
+Roseville, CA, US
+Sacramento, CA, US
+Saginaw, MI, US
+Saint Louis, MO, US
+St. Louis, MO, US
+Saint Paul, MN, US
+Salem, OR, US
+Salinas, CA, US
+Salt Lake City, UT, US
+San Antonio, TX, US
+San Bernardino, CA, US
+San Buenaventura, CA, US
+San Diego, CA, US
+San Francisco, CA, US
+San Jose, CA, US
+Santa Ana, CA, US
+Santa Barbara, CA, US
+Santa Clara, CA, US
+Santa Cruz, CA, US
+Santa Maria, CA, US
+Sarasota, FL, US
+Savannah, GA, US
+Scottsdale, AZ, US
+Scranton, PA, US
+Seaside, OR, US
+Seattle, WA, US
+Sebastian, FL, US
+Shreveport, LA, US
+Simi Valley, CA, US
+Sioux City, IA, US
+Sioux Falls, SD, US
+South Bend, IN, US
+South Lyon, MI, US
+Spartanburg, SC, US
+Spokane, WA, US
+Springdale, AR, US
+Springfield, MO, US
+Stamford, CT, US
+Sterling Heights, MI, US
+Stockton, CA, US
+Sunnyvale, CA, US
+Syracuse, NY, US
+Tacoma, WA, US
+Tallahassee, FL, US
+Tampa, FL, US
+Tempe, AZ, US
+Thornton, CO, US
+Thousand Oaks, CA, US
+Toledo, OH, US
+Topeka, KS, US
+Torrance, CA, US
+Trenton, NJ, US
+Tucson, AZ, US
+Tulsa, OK, US
+Tuscaloosa, AL, US
+Utica, NY, US
+Vallejo, CA, US
+Virginia Beach, VA, US
+Waco, TX, US
+Warren, MI, US
+Washington, DC, US
+Waterloo, IA, US
+Westminster, CO, US
+Wichita, KS, US
+Winston, NC, US
+Winter Haven, FL, US
+Worcester, MA, US
+Yakima, WA, US
+Yonkers, NY, US
+York, PA, US
+St. Charles, MO, US
+St. Peters, MO, US
+Chesterfield, MO, US
+Washington, MO, US
+Youngstown, OH, US`.split('\n');
